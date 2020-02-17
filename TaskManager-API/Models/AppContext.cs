32df39adefc0eecec58b86f8a0c311df689a9342
EvoFint <evofint@gmail.com>
2020-02-17 22:44:22 +0700

    Add AppContext.cs for connect to data base

---
+++ TaskManager-API/Models/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager_API.Models
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=task_manager_db;Username=postgres;Password=Evofint8146");
+        }
+    }
+}
