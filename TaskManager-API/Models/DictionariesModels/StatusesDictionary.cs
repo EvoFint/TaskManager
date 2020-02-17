e448621e64fbcb62f14edaf338f88463040ec4ba
EvoFint <evofint@gmail.com>
2020-02-17 22:53:19 +0700

    Add model for statuses dictionary

---
+++ TaskManager-API/Models/DictionariesModels/StatusesDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager_API.Models.DictionariesModels
+{
+    [Table("Statuses")]
+    public class StatusesDictionary
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        
+        [Column("status_name")]
+        public string Name { get; set; }
+    }
+}
