0134804da98b073b8dd93730d5d82300ca894a2a
EvoFint <evofint@gmail.com>
2020-02-17 23:20:50 +0700

    Add api getTaskStatuses

---
+++ TaskManager-API/Controllers/DictionariesControllers/StatusesDictionaryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager_API.Models;
+
+namespace TaskManager_API.Controllers.DictionariesControllers
+{
+    [ApiController]
+    public class StatusesDictionaryController : Controller
+    {
+        [Route("dictionariesController/getTasksStatuses")]
+        public async Task<IActionResult> GetTasksStatuses(string token)
+        {
+            using (TaskManagerAppContext db = new TaskManagerAppContext())
+            {
+                var statusesDictionary = await db.StatusesDictionaries.ToListAsync();
+                return Json(statusesDictionary.Select(sd => new {id = sd.Id, name = sd.Name}));
+            }
+        }
+    }
+}
