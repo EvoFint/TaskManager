b99f9d5b458bbca51810559290bb982fe9f6312c
EvoFint <evofint@gmail.com>
2020-02-22 22:19:12 +0700

    Create StatusesDto.cs

---
+++ TaskManager.Backend/Models/DTO/StatusesDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManager.Backend.Models.DTO
+{
+    public class StatusesDto
+    {
+        [JsonPropertyName("id")] 
+        private int Id { get; }
+        
+        [JsonPropertyName("name")]
+        private string Name { get; }
+
+        public StatusesDto(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
