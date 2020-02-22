9d932614efc9e4170fb126656fa8f17f47a844e7
EvoFint <evofint@gmail.com>
2020-02-22 22:08:51 +0700

    Create dto for requests

---
+++ TaskManager.Backend/Models/DTO/RequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManager.Backend.Models.DTO
+{
+    public class RequestDto<T>
+    {
+        [JsonPropertyName("data")]
+        T Data { get; }
+
+        public RequestDto(T data)
+        {
+            Data = data;
+        }
+    }
+}
