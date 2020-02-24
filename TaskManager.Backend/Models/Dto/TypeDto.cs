de127c6c501533734ad2876fe313c40b5ea404c5
EvoFint <evofint@gmail.com>
2020-02-24 18:37:05 +0700

    Create type dto

---
+++ TaskManager.Backend/Models/Dto/TypeDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using TaskManager.Backend.Models.DictionariesModels;
+
+namespace TaskManager.Backend.Models.Dto
+{
+    public class TypeDto
+    {
+        [JsonProperty("id")]
+        private int Id { get; }
+        
+        [JsonProperty("name")]
+        private string Name { get; }
+
+        public TypeDto(TypesDictionary item)
+        {
+            Id = item.Id;
+            Name = item.Name;
+        }
+        
+        public static TypeDto FromDocument(TypesDictionary doc) => new TypeDto(doc);
+    }
+}
