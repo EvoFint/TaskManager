b85cd8f1f66020a494c1f9085ff37784751b0cae
EvoFint <evofint@gmail.com>
2020-02-24 20:29:47 +0700

    Create controller TypesDictionaryController.cs

---
+++ TaskManager.Backend/Controllers/DictionariesControllers/TypesDictionaryController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Backend.Models.Dto;
+using TaskManager.Backend.Services;
+
+namespace TaskManager.Backend.Controllers.DictionariesControllers
+{
+    [Route("api/dictionaries")]
+    [ApiController]
+    public class TypesDictionaryController : Controller
+    {
+        private readonly ITypesService _typesService;
+
+        public TypesDictionaryController(ITypesService typesService)
+        {
+            _typesService = typesService;
+        }
+
+        [HttpGet, Route("types")]
+        public async Task<TypeDto[]> GetTasksTypes(string token) => await _typesService.GetAll();
+    }
+}
