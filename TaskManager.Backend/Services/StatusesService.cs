737b2b1b8c9075c9fdcb47c9a067959c245a7bec
EvoFint <evofint@gmail.com>
2020-02-24 17:50:35 +0700

    Rename dto -> dtos, change task type

--- TaskManager.Backend/Services/StatusesService.cs
+++ TaskManager.Backend/Services/StatusesService.cs
@@ -10,11 +10,11 @@ namespace TaskManager.Backend.Services
     public class StatusesService : IStatusesService
     {
         TaskManagerAppContext db = new TaskManagerAppContext();
-        public async Task<StatusesListDto> GetAll()
+        public async Task<StatusDto[]> GetAll()
         {
             var statusesDictionary = await db.StatusesDictionaries.ToListAsync();
-            var dto = statusesDictionary.Select(StatusDto.FromDocument);
-            return new StatusesListDto{Items = dto.ToArray()};
+            var dtos = statusesDictionary.Select(StatusDto.FromDocument).ToArray();
+            return dtos;
         }
     }
 }
