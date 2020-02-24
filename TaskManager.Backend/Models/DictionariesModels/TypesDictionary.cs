f29edc4249637a93350ee71b623cf26f8b730aa4
EvoFint <evofint@gmail.com>
2020-02-24 18:24:50 +0700

    Create TypesDictionary.cs model for types table

---
+++ TaskManager.Backend/Models/DictionariesModels/TypesDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Backend.Models.DictionariesModels
+{
+    [Table("types")]
+    public class TypesDictionary
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        
+        [Column("type_name")]
+        public int Name { get; set; }
+    }
+}
