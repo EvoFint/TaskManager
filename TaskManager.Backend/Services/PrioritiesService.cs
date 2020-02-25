6e2451f7452053d92157f65dabb62f42e32f01dc
EvoFint <evofint@gmail.com>
2020-02-25 22:25:39 +0700

    Create PrioritiesService.cs

---
+++ TaskManager.Backend/Services/PrioritiesService.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Backend.Models.Context;
+using TaskManager.Backend.Models.Dto;
+
+namespace TaskManager.Backend.Services
+{
+    public class PrioritiesService : IPrioritiesService
+    {
+        TaskManagerAppContext db = new TaskManagerAppContext();
+        public async Task<PriorityDto[]> GetAll()
+        {
+            var prioritiesDictionary = await db.PrioritiesDictionaries.ToListAsync();
+            var dtos = prioritiesDictionary.Select(PriorityDto.FromDocument).ToArray();
+            return dtos;
+        }
+    }
+}
