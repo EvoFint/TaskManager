fc6efb98bae865716fbb1520c7b22b7904fabc8b
EvoFint <evofint@gmail.com>
2020-02-25 22:22:49 +0700

    Create IPrioritiesService.cs

---
+++ TaskManager.Backend/Services/IPrioritiesService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TaskManager.Backend.Models.Dto;
+
+namespace TaskManager.Backend.Services
+{
+    public interface IPrioritiesService
+    {
+        Task<PriorityDto[]> GetAll();
+    }
+}
