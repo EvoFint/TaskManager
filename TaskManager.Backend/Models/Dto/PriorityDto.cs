0e1bc5eb3e94efa7c3a2f2130d04d83f30988638
EvoFint <evofint@gmail.com>
2020-02-25 22:20:16 +0700

    Create dto model PriorityDto.cs

---
+++ TaskManager.Backend/Models/Dto/PriorityDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using TaskManager.Backend.Models.DictionariesModels;
+
+namespace TaskManager.Backend.Models.Dto
+{
+    public class PriorityDto
+    {
+        [JsonProperty("id")]
+        private int Id { get; }
+        
+        [JsonProperty("name")]
+        private string Name { get; }
+
+        public PriorityDto(PrioritiesDictionary item)
+        {
+            Id = item.Id;
+            Name = item.Name;
+        }
+        
+        public static PriorityDto FromDocument(PrioritiesDictionary doc) => new PriorityDto(doc);
+    }
+}
