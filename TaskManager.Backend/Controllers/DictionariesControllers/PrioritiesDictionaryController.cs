0bfecc98a22cf39c2ed1e178dd9748919b9c9fae
EvoFint <evofint@gmail.com>
2020-02-25 22:31:10 +0700

    Create PrioritiesDictionaryController.cs

---
+++ TaskManager.Backend/Controllers/DictionariesControllers/PrioritiesDictionaryController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Backend.Models.Dto;
+using TaskManager.Backend.Services;
+
+namespace TaskManager.Backend.Controllers.DictionariesControllers
+{
+    [Route("api/dictionaries")]
+    [ApiController]
+    public class PrioritiesDictionaryController : Controller
+    {
+        private readonly IPrioritiesService _prioritiesService;
+        
+        public PrioritiesDictionaryController(IPrioritiesService prioritiesService)
+        {
+            _prioritiesService = prioritiesService;
+        }
+
+        [HttpGet, Route("priorities")]
+        public async Task<PriorityDto[]> GetTasksPriorities(string token) => await _prioritiesService.GetAll();
+    }
+}
