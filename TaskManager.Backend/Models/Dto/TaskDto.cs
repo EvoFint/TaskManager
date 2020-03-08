bcc7b8054264477de4990ec50abfac7eda1d257e
EvoFint <evofint@gmail.com>
2020-03-08 16:08:03 +0700

    Create TaskDto.cs

---
+++ TaskManager.Backend/Models/Dto/TaskDto.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace TaskManager.Backend.Models.Dto
+{
+    public class TaskDto
+    {
+        [JsonProperty("id")]
+        private int Id { get; }
+        
+        [JsonProperty("boardId")]
+        private int BoardId { get; }
+        
+        [JsonProperty("name")]
+        private string Name { get; }
+        
+        [JsonProperty("parentItemId")]
+        private int ParentItemId { get; }
+        
+        [JsonProperty("typeId")]
+        private int TypeId { get; }
+        
+        [JsonProperty("description")]
+        private string Description { get; }
+        
+        [JsonProperty("statusId")]
+        private int StatusId { get; }
+        
+        [JsonProperty("storyPoints")]
+        private int StoryPoints { get; }
+
+        public TaskDto(TaskModel item)
+        {
+            Id = item.Id;
+            BoardId = item.BoardId;
+            Name = item.Name;
+            ParentItemId = item.ParentItemId;
+            TypeId = item.TypeId;
+            Description = item.Description;
+            StatusId = item.StatusId;
+            StoryPoints = item.StoryPoint;
+        }
+        
+        public static TaskDto FromDocument(TaskModel doc) => new TaskDto(doc);
+    }
+}
