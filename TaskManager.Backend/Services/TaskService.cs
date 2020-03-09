328238ab5ff0054b3eff81a5a6190cdf1ae67d76
EvoFint <evofint@gmail.com>
2020-03-09 15:31:26 +0700

    Add checker of null

--- TaskManager.Backend/Services/TaskService.cs
+++ TaskManager.Backend/Services/TaskService.cs
@@ -10,6 +10,10 @@ namespace TaskManager.Backend.Services
         
         public async Task<string> CreateNewTask(TaskModel task)
         {
+            if (task == null)
+            {
+                return "task can not to be null";
+            }
             db.Tasks.Add(task);
             await db.SaveChangesAsync();
             return "Ok";
